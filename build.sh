d29599fd3af405a802c9db277a9dc98de160117d
lee2sman <leetusman@gmail.com>
2021-04-18 02:11:25 -0400

    update build system

--- build.sh
+++ build.sh
@@ -1,1 +1,14 @@
- pandoc -c assets/css/style.css -t html5 --metadata title="now" -s README.md -o index.html
+#################################
+#                               #
+#                               #
+#    Build system for /now      #
+#                               #
+#                               #
+#################################
+
+pandoc -c assets/css/style.css -t html5 --metadata title="now" -s README.md -o index.html
+
+# save it
+git add README.md index.html
+git commit -m 'update now'
+git push
